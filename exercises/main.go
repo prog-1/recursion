d141dce38d73c063482765c1c237e645d5f51cae
Ilya <imolchanov210@gmail.com>
2022-09-25 21:10:16 +0300

    another variant, there is still a bug

--- exercises/main.go
+++ exercises/main.go
@@ -1,24 +1,37 @@
 package main
 
+import "fmt"
+
 func partition(s []int, pivot int) (i int) {
-	l, r := 0, len(s)-1
+
 	p := s[pivot]
 	for {
-		for s[l] < p {
+		fmt.Println(s)
+		l, r := 0, len(s)-1
+
+		for s[l] <= p {
 			l++
 		}
-		for s[r] >= p && r > 0 {
+		for s[r] > p && r > 0 {
 			r--
 		}
+
 		if l >= r && r > 0 {
-			return r + 1
+			fmt.Println(s)
+			return r
 		} else if l >= r && r <= 0 {
 			return r
 		}
 		s[l], s[r] = s[r], s[l]
+
 	}
+
 }
 
 func main() {
-
+	s := []int{9, 6, 1, 7, 5, 9, 6, 5, 2, 6}
+	i := partition(s, 9)
+	fmt.Println("Output: ", i, s)
+	fmt.Println("Left side: ", s[:i])
+	fmt.Println("Right side: ", s[i:])
 }
