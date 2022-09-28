770c17113a3699c0df5201eeb1c75b4a12396b6e
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2022-09-28 23:06:04 +0300

    Added 2nd Numbers excercise

--- exercises/main.go
+++ exercises/main.go
@@ -2,38 +2,27 @@ package main
 
 import "fmt"
 
-func rearrange(s []int, pivot int) (i int) {
-	l, r := 0, len(s)-1
-	p := s[pivot]
-	for {
-		for ; s[l] < p; l++ {
-		}
-		for ; s[r] >= p && r > 0; r-- {
-		}
-		if l >= r {
-			if r == 0 {
-				return r
-			} else {
-				return r + 1
-			}
-		}
+func numbers1(n int) {
 
-		s[l], s[r] = s[r], s[l]
+	fmt.Println(n)
 
-		l++
-		r--
+	if n > 1 {
+		numbers1(n-1)
 	}
+
 }
 
+func numbers2(n int) {
+
+	if n > 1 {
+		numbers2(n-1)
+	}
+
+	fmt.Println(n)
+
+}
+
+
 func main() {
-	p := 9
-	fmt.Println("Pivot:", p)
-	s := []int{9, 6, 1, 7, 5, 9, 6, 5, 2, 6}
-	fmt.Println("Initial:", s)
-	fmt.Println()
-	i := rearrange(s, p)
-	fmt.Println("Index:", i)
-	fmt.Println("Processed:", s)
-	fmt.Println("s[:i]:", s[:i])
-	fmt.Println("s[i:]:", s[i:])
+	numbers1(10)
 }
