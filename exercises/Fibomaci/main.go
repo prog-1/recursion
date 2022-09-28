4cac6d4fde21d3c6ab7dd9701f8e927d551da207
Ilya <imolchanov210@gmail.com>
2022-09-28 19:31:05 +0300

    fix

--- exercises/Fibomaci/main.go
+++ exercises/Fibomaci/main.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 )
 
-var s = []int{0, 1}
-var cnt = 1
-
-func cycle(n int) []int {
+func cycle(n, cnt int, s []int) []int {
 	s = append(s, s[len(s)-1]+s[len(s)-2])
 	cnt++
 	if cnt != n {
-		cycle(n)
+		cycle(n, cnt, s)
 	}
 	return s
 }
@@ -23,7 +20,9 @@ func fib(n int) []int {
 	} else if n == 1 {
 		return []int{0, 1}
 	}
-	return cycle(n)
+	cnt := 1
+	var s = []int{0, 1}
+	return cycle(n, cnt, s)
 }
 func main() {
 	var n int
